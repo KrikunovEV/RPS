6e0cd277f710c9b30589711182e9a427575004d3
Evgenii Krikunov <Jen9wka@gmail.com>
2020-11-01 23:44:54 +0300

    correct metrics

--- Agent.py
+++ Agent.py
@@ -1,5 +1,5 @@
-from Model import PredictionModel, GenerationModel
 import torch
+from Model import PredictionModel, GenerationModel
 from enum import Enum
 
 
@@ -8,20 +8,22 @@ class Agent:
         Categorical = 1
         Numerical = 2
 
-    def __init__(self, label: str, message_type: MessageType, lr: float, obs_space: int, action_space: int):
+    def __init__(self, label: str, message_type: MessageType, lr: float, obs_space: int, action_space: int, steps: int):
         self.agent_label = label + (' categorical' if message_type == self.MessageType.Categorical else ' numerical')
         self.message_type = message_type
+        self.action_space = action_space
 
         self.generator = GenerationModel(obs_space=obs_space, action_space=action_space)
         self.predictor = PredictionModel(obs_space=obs_space, action_space=action_space)
-        self.optimizer = torch.optim.SGD(list(self.generator.parameters()) + list(self.predictor.parameters()), lr=lr)
+        self.optimizer = torch.optim.Adam(list(self.generator.parameters()) + list(self.predictor.parameters()), lr=lr)
         self.loss_fn = torch.nn.MSELoss() if message_type == self.MessageType.Numerical else torch.nn.CrossEntropyLoss()
 
-        self.generated_messages = [torch.zeros(20)]
+        self.generated_messages = [torch.zeros(self.action_space)]
         self.received_messages = []
 
         self.loss_metric = []
         self.accuracy_metric = []
+        self.accuracy_by_step_metric = torch.zeros(steps - 1)
 
     def get_last_message(self):
         return self.generated_messages[-1].detach()  # do detach to make sure that gradient won't be corrupted
@@ -44,19 +46,33 @@ class Agent:
             target = target.argmax(dim=1)
 
         loss = self.loss_fn(predicted, target)
+        self.loss_metric.append(loss.item())
+
         if self.message_type == self.MessageType.Categorical:
             accuracy = torch.mean(predicted.argmax(dim=1) == target, dtype=torch.float)
-        else:
-            accuracy = torch.mean(predicted.argmax(dim=1) == target.argmax(dim=1), dtype=torch.float)
+            self.accuracy_metric.append(accuracy.item())
 
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-        self.loss_metric.append(loss.item())
-        self.accuracy_metric.append(accuracy.item())
+        self.generated_messages = [torch.zeros(self.action_space)]
+        self.received_messages = []
 
-        self.generated_messages = [torch.zeros(20)]
+    def eval(self):
+        self.generated_messages = self.generated_messages[:-2]  # nothing to predict for the last messages
+        self.received_messages = self.received_messages[1:]  # first message is zero tensor
+
+        x = torch.stack(self.generated_messages)
+        predicted = self.predictor(x)
+        target = torch.stack(self.received_messages)
+        if self.message_type == self.MessageType.Categorical:
+            target = target.argmax(dim=1)
+
+        if self.message_type == self.MessageType.Categorical:
+            self.accuracy_by_step_metric += predicted.argmax(dim=1) == target
+
+        self.generated_messages = [torch.zeros(self.action_space)]
         self.received_messages = []
 
     '''
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 from Agent import Agent
+import numpy as np
 
 
-negotiations = 5000
+negotiations = 1000
 steps = 10
 lr = 0.001
 
-A = Agent(label='A', message_type=Agent.MessageType.Categorical, lr=lr, obs_space=20, action_space=20)
-B = Agent(label='B', message_type=Agent.MessageType.Categorical, lr=lr, obs_space=20, action_space=20)
+A = Agent(label='A', message_type=Agent.MessageType.Categorical, lr=lr, obs_space=20, action_space=20, steps=steps)
+B = Agent(label='B', message_type=Agent.MessageType.Categorical, lr=lr, obs_space=20, action_space=20, steps=steps)
 
 for negotiation in range(negotiations):
     print(f'negotiation: {negotiation + 1}/{negotiations}')
@@ -24,21 +25,49 @@ for negotiation in range(negotiations):
     A.train()
     B.train()
 
-fig, ax = plt.subplots(1, 2, figsize=(16, 9))
+test_negotiations = 1000
+for negotiation in range(test_negotiations):
+    print(f'test negotiation: {negotiation + 1}/{test_negotiations}')
+
+    Am = A.get_last_message()
+    Bm = B.get_last_message()
+
+    for step in range(steps):
+        Am_prev = Am
+        Bm_prev = Bm
+        Am = A.generate_message(message=Bm_prev)
+        Bm = B.generate_message(message=Am_prev)
+
+    A.eval()
+    B.eval()
+
+fig, ax = plt.subplots(1, 3, figsize=(16, 9))
 
 ax[0].set_title(f'Loss')
-ax[0].plot(A.loss_metric, label=A.agent_label)
-ax[0].plot(B.loss_metric, label=B.agent_label)
+ax[0].plot(np.convolve(A.loss_metric, np.full(15, 1./15.)), label=A.agent_label)
+ax[0].plot(np.convolve(B.loss_metric, np.full(15, 1./15.)), label=B.agent_label)
 ax[0].set_xlabel('# of negotiation')
 ax[0].set_ylabel('value')
 ax[0].legend()
 
 ax[1].set_title(f'Accuracy')
-ax[1].plot(A.accuracy_metric, label=A.agent_label)
-ax[1].plot(B.accuracy_metric, label=B.agent_label)
+if A.message_type == Agent.MessageType.Categorical:
+    ax[1].plot(np.convolve(A.accuracy_metric, np.full(15, 1./15.)), label=A.agent_label)
+if B.message_type == Agent.MessageType.Categorical:
+    ax[1].plot(np.convolve(B.accuracy_metric, np.full(15, 1./15.)), label=B.agent_label)
 ax[1].set_xlabel('# of negotiation')
 ax[1].set_ylabel('value')
 ax[1].legend()
+
+ax[2].set_title(f'Accuracy by step')
+if A.message_type == Agent.MessageType.Categorical:
+    ax[2].plot(np.convolve(A.accuracy_by_step_metric / test_negotiations, np.full(15, 1./15.)), label=A.agent_label)
+if B.message_type == Agent.MessageType.Categorical:
+    ax[2].plot(np.convolve(B.accuracy_by_step_metric / test_negotiations, np.full(15, 1./15.)), label=B.agent_label)
+ax[2].set_xlabel('negotiation step')
+ax[2].set_ylabel('value')
+ax[2].legend()
+
 
 plt.tight_layout()
 plt.show()
