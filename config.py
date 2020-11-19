9b0a9427df4b81994ebe5f5e04cb27d615039f80
Evgenii Krikunov <Jen9wka@gmail.com>
2020-11-19 22:27:36 +0300

    Change message type on Numberical because Categorical doesn't make sense

--- config.py
+++ config.py
@@ -8,10 +8,11 @@ cfg.n_agents = 1  # number of agents who won't negotiate
 cfg.lr = 0.001
 cfg.train_episodes = 1000
 cfg.test_episodes = 200
+cfg.gamma = 0.99
 
 cfg.negot = EasyDict()
 cfg.negot.teams = [2]  # list of number of agents who negotiate within a team
-cfg.negot.message_type = MessageType.Categorical
+cfg.negot.message_type = MessageType.Numerical
 cfg.negot.message_space = 10
 cfg.negot.lr = 0.001
 cfg.negot.steps = 10
