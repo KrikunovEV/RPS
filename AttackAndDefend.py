f068f802147ec56a4d4741c66f72d8c7c77acb21
Evgenii Krikunov <Jen9wka@gmail.com>
2021-03-11 10:47:14 +0300

    refactor config and environment (add empty one-hot value)

--- AttackAndDefend.py
+++ AttackAndDefend.py
@@ -4,23 +4,6 @@ import numpy as np
 class AADEnvironment:
     """
     Игроки должны выбрать кого атаковать и от кого защищаться.
-    Возможны 4 исхода для игроков i и j:
-    - (Ничья)     Если игрок i атаковал игрока j,           а j защитился    от i,     то j выживет.
-    - (Выживание) Если игрок i атаковал игрока j,           а j не защитился от i,     то j проиграет.
-    - (Выживание) Если игрок i защитился от игрока j,       а j атаковал i,            то i выживет
-    - (Поражение) Если игрок i не защитился от игрока j,    а j атаковал i,            то i проиграет
-
-    action (2)
-    Действие представляет из себя два числа
-
-    obs (players * (players * action))
-    Кодируем прошедший раунд one-hot векторами.
-
-    choices (players, action)
-    Каждый игрок делает 2 действия
-
-    reward (players)
-    Награда делится между выжившими игроками
     """
 
     def __init__(self, players: int, debug: bool = False):
@@ -37,6 +20,7 @@ class AADEnvironment:
         We just need to check that all players have defended.
         If someone not, do reward[someone] = 0
         """
+
         rewards = np.ones(self.players)
         for p in range(self.players):
             attacked = choices[p][0]
@@ -48,12 +32,9 @@ class AADEnvironment:
 
         obs = np.zeros(self.obs_space)
         for p in range(self.players):
-            obs[p * 2 * self.players + choices[p][0]] = 1.
-            obs[p * 2 * self.players + self.players + choices[p][1]] = 1.
+            obs[p * 2 * (self.players + 1) + choices[p][0]] = 1.
+            obs[p * 2 * (self.players + 1) + (self.players + 1) + choices[p][1]] = 1.
 
-        total_reward = rewards.sum()
-        if total_reward != 0:
-            rewards = rewards / total_reward
         rewards[rewards == 0] = -1.0
         self.__print(rewards)
 
--- config.py
+++ config.py
@@ -1,16 +1,13 @@
-from easydict import EasyDict
 
 
-cfg = EasyDict()
-cfg.n_agents = 1  # number of agents who won't negotiate
-cfg.train_episodes = 1000
-cfg.test_episodes = 500
-cfg.lr = 0.005
-cfg.gamma = 0.9
-cfg.entropy_coef = 0.001
+n_agents = 1  # number of agents who won't negotiate
+train_episodes = 1000
+test_episodes = 500
+lr = 0.005
+gamma = 0.9
+entropy_coef = 0.001
 
-cfg.negot = EasyDict()
-cfg.negot.n_agents = 2  # number of agents who will negotiate
-cfg.negot.steps = 2
+negot_n_agents = 2  # number of agents who will negotiate
+negot_steps = 2
 
-cfg.players = cfg.n_agents + cfg.negot.n_agents
+players = n_agents + negot_n_agents
