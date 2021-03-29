b61fd7fa2285a1353c600ceab5cc841f9afdaaf6
Evgenii Krikunov <evgenii.krikunov@pr3vision.com>
2021-03-29 14:17:58 +0300

    add corrections to main pipeline

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-from AttackAndDefend import AADEnvironment
+from OffendAndDefend import OADEnvironment
 from AgentOrchestrator import Orchestrator
 import config as cfg
 import time
 import os
 
 
-def main(id, model_type: cfg.ModelType, log: cfg.LogType):
-    env = AADEnvironment(players=cfg.players)
-    orchestrator = Orchestrator(obs_space=env.get_obs_space(), action_space=env.get_action_space(),
+def run(epoch, model_type: cfg.ModelType, debug: bool = False):
+
+    if model_type == cfg.ModelType.attention and not cfg.use_negotiation and not cfg.use_embeddings:
+        raise Exception(f'You can not use attention model if negotiation ({cfg.use_negotiation}) and embedding '
+                        f'({cfg.use_embeddings}) are disabled both.')
+
+    env = OADEnvironment(players=cfg.players)
+    orchestrator = Orchestrator(obs_space=env.get_obs_space(),
+                                action_space=env.get_action_space(),
                                 model_type=model_type, cfg=cfg)
 
     orchestrator.set_eval(eval=False)
-    obs = env.reset()
     epsilon = cfg.epsilon_upper
+    obs = None
     for episode in range(cfg.train_episodes):
-        #print(f'Train episode: {episode + 1}/{cfg.train_episodes}')
-        if episode != 0 and episode % cfg.test_episodes == 0:
+        if debug:
+            print(f'Epoch: {epoch}, train episode: {episode + 1}/{cfg.train_episodes}')
+
+        if episode % cfg.round_episodes == 0:
             obs = env.reset()
-            if model_type == cfg.ModelType.baseline_rnn:
+            if cfg.is_require_reset(model_type):
                 orchestrator.reset_h()
+
         if cfg.shuffle:
+            raise Exception('FIX IT: obs should be shuffled correspondingly')
             orchestrator.shuffle()
+
         if cfg.use_negotiation:
             orchestrator.negotiation(obs)
+
         choices = orchestrator.decisions(obs, epsilon)
         obs, rewards = env.play(choices)
         orchestrator.rewarding(rewards)
@@ -30,47 +42,54 @@ def main(id, model_type: cfg.ModelType, log: cfg.LogType):
         epsilon -= cfg.epsilon_step
 
     orchestrator.set_eval(eval=True)
-    obs = env.reset()
-    orchestrator.reset_h()
+    obs = None
     result = 0
     for episode in range(cfg.test_episodes):
-        # print(f'Test episode: {episode + 1}/{cfg.test_episodes}')
+        if debug:
+            print(f'Epoch: {epoch}, test episode: {episode + 1}/{cfg.test_episodes}')
+
+        if episode % cfg.round_episodes == 0:
+            obs = env.reset()
+            if cfg.is_require_reset(model_type):
+                orchestrator.reset_h()
+
         if cfg.shuffle:
             orchestrator.shuffle()
+
         if cfg.use_negotiation:
             orchestrator.negotiation(obs)
+
         choices = orchestrator.decisions(obs, epsilon)
         obs, rewards = env.play(choices)
         orchestrator.rewarding(rewards)
 
+        raise Exception('FIX IT')
         if rewards[2] > rewards[0] and rewards[1] > rewards[0]:
             result += 1
 
     if log == cfg.LogType.show:
         orchestrator.plot_metrics(directory=None)
     elif log == cfg.LogType.local:
+        if not os.path.exists(cfg.metric_directory):
+            os.mkdir(cfg.metric_directory)
         directory = os.path.join(cfg.metric_directory, model_type.name)
         if not os.path.exists(directory):
             os.mkdir(directory)
-        directory = os.path.join(directory, f'{id}')
+        directory = os.path.join(directory, f'{epoch}')
         if not os.path.exists(directory):
             os.mkdir(directory)
         orchestrator.plot_metrics(directory=directory)
     elif log == cfg.LogType.mlflow:
-        pass
+        print(f'Log type mlflow not implemented. Logs are not saved.')
 
     return result
 
 
 if __name__ == '__main__':
-    if not cfg.Train:
-        print(f'The value of cfg.Train is {cfg.Train}. Is this expected?')
-
-    print(f'Use negotiation: {cfg.use_negotiation}')
-    print(f'Use embeddings: {cfg.use_embeddings}')
+    cfg.print_config()
 
     start_time = time.time()
-    coop_result = main('test', cfg.ModelType.attention, cfg.LogType.show)
+    coop_result = run('test', cfg.ModelType.attention)
 
     print(f'Time: {time.time() - start_time}')
     print(f'coop: {coop_result}/{cfg.test_episodes}')
