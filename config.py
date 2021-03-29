c60cfdd6758778a88f55fa6950a7fecb268f7438
Evgenii Krikunov <evgenii.krikunov@pr3vision.com>
2021-03-29 13:09:31 +0300

    add printing config params

--- config.py
+++ config.py
@@ -49,3 +49,48 @@ agents = 1  # number of agens who won't negotiate
 players = agents + negotiable_agents
 shuffle = True
 metric_directory = 'metrics'
+
+
+def print_config():
+    from sty import fg, ef, Style, RgbFg
+    fg.orange = Style(RgbFg(255, 165, 0))
+    fg.orange_dark = Style(RgbFg(229, 83, 0))
+    fg.red_war = Style(RgbFg(255, 10, 10))
+
+    print('\n' + fg.orange_dark + 'Multiprocessing:' + fg.rs)
+    print(fg.orange + 'Cores: ' + fg.rs + f'{cores}')
+    print(fg.orange + 'Epochs: ' + fg.rs + f'{epochs}')
+
+    print('\n' + fg.orange_dark + 'Episodes:' + fg.rs)
+    print(fg.orange + 'Train episodes: ' + fg.rs + f'{train_episodes}')
+    print(fg.orange + 'Test episodes: ' + fg.rs + f'{test_episodes}')
+    print(fg.orange + 'Rnn episodes: ' + fg.rs + f'{rnn_episodes}')
+
+    print('\n' + fg.orange_dark + 'Learning:' + fg.rs)
+    print(fg.orange + 'Train: ' + fg.rs + f'{Train} ' + ('' if Train else (fg.red_war + 'Is this expected?!' + fg.rs)))
+    print(fg.orange + 'Learning rate: ' + fg.rs + f'{lr}')
+    print(fg.orange + 'Hidden size: ' + fg.rs + f'{hidden_size}')
+    print(fg.orange + 'Gamma: ' + fg.rs + f'{gamma}')
+
+    print('\n' + fg.orange_dark + 'Exploration:' + fg.rs)
+    print(fg.orange + 'Entropy coef: ' + fg.rs + f'{entropy_coef}')
+    print(fg.orange + 'Epsilon upper: ' + fg.rs + f'{epsilon_upper}')
+    print(fg.orange + 'Epsilon lower: ' + fg.rs + f'{epsilon_lower}')
+    print(fg.orange + 'Epsilon step: ' + fg.rs + f'{round(epsilon_step, 7)}')
+
+    print('\n' + fg.orange_dark + 'Negotiation:' + fg.rs)
+    print(fg.orange + 'Use negotiation: ' + fg.rs + f'{use_negotiation}')
+    print(fg.orange + 'Use embeddings: ' + fg.rs + f'{use_embeddings}')
+    print(fg.orange + 'Is channel open: ' + fg.rs + f'{is_channel_open}')
+    print(fg.orange + 'Negotiable agents: ' + fg.rs + f'{negotiable_agents}')
+    print(fg.orange + 'Negotiation steps: ' + fg.rs + f'{negotiation_steps}')
+
+    print('\n' + fg.orange_dark + 'Common:' + fg.rs)
+    print(fg.orange + 'Agents: ' + fg.rs + f'{agents}')
+    print(fg.orange + 'Players: ' + fg.rs + f'{players}')
+    print(fg.orange + 'Shuffle: ' + fg.rs + f'{shuffle}')
+    print(fg.orange + 'Metric directory: ' + fg.rs + f'{metric_directory}')
+
+
+if __name__ == '__main__':
+    print_config()
