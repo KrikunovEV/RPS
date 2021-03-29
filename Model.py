6d9f2f3d0d6ac7b78b55d9214d371ab8632d88b4
Evgenii Krikunov <Jen9wka@gmail.com>
2021-03-29 22:56:53 +0300

    fix Models

--- Model.py
+++ Model.py
@@ -21,66 +21,73 @@ class NegotiationModel(nn.Module):
 
 class AttentionModel(nn.Module):
 
-    def __init__(self, obs_space: int, action_space: int, message_space: int, embedding_space: int, hidden_size: int,
-                 cfg):
+    def __init__(self, obs_space: int, action_space: int, message_space: int, cfg):
         super(AttentionModel, self).__init__()
-
         self.cfg = cfg
-        self.message_space = message_space
-        self.n_players = cfg.n_players
-        self.hidden_size = hidden_size
 
         if cfg.use_negotiation:
             obs_space += message_space
 
         if cfg.use_embedding:
-            obs_space += embedding_space
+            obs_space += cfg.embedding_space
 
-        self.rnn = nn.GRUCell(rnn_in_space, hidden_size)
+        self.rnn = nn.GRUCell(obs_space, cfg.hidden_size)
         self.ff = nn.Sequential(
-            nn.Linear(obs_space + hidden_size, 2),
+            nn.Linear(cfg.hidden_size, 2),
             nn.LeakyReLU()
         )
 
-        self.a_policy = nn.Linear(hidden_size, n_players)
-        self.d_policy = nn.Linear(hidden_size, n_players)
-        self.V = nn.Linear(hidden_size, 1)
+        self.a_policy = nn.Linear(cfg.hidden_size, action_space)
+        self.d_policy = nn.Linear(cfg.hidden_size, action_space)
+        self.V = nn.Linear(cfg.hidden_size, 1)
 
-    def forward(self, obs, messages):
-        h = [torch.zeros(self.hidden_size)]
+    def get_h(self):
+        return None
+
+    def forward(self, obs, messages, embeddings, h):
+        h = [torch.zeros((1, self.cfg.hidden_size))]
         actions = []
-        for p in range(self.n_players):
-            h.append(self.rnn(messages[p * self.message_space:(p + 1) * self.message_space].unsqueeze(0),
-                              h[-1].unsqueeze(0))[0])
-            actions.append(self.ff(torch.cat((obs, h[-1]))))
+        for p in range(self.cfg.players):
+            agent_obs = obs[p]
+            if self.cfg.use_negotiation:
+                agent_obs = torch.cat((agent_obs, messages[p]))
+            if self.cfg.use_embedding:
+                agent_obs = torch.cat((agent_obs, embeddings[p]))
+            h.append(self.rnn(agent_obs.unsqueeze(0), h[-1]))
+            actions.append(self.ff(h[-1][0]))
 
         actions = torch.cat(actions)
         h = torch.stack(h[1:])
-        attack_attention = torch.sum(torch.softmax(actions[::2], -1).reshape(-1, 1) * h, dim=0)
-        defend_attention = torch.sum(torch.softmax(actions[1::2], -1).reshape(-1, 1) * h, dim=0)
+        a_attention = torch.sum(torch.softmax(actions[::2], -1).reshape(-1, 1) * h, dim=0)
+        d_attention = torch.sum(torch.softmax(actions[1::2], -1).reshape(-1, 1) * h, dim=0)
 
-        return self.a_policy(attack_attention), self.d_policy(defend_attention), self.V(attack_attention + defend_attention)
+        return self.a_policy(a_attention), self.d_policy(d_attention), self.V(a_attention + d_attention), None
 
 
 class BaselineRNNModel(nn.Module):
 
-    def __init__(self, obs_space: int, action_space: int, message_space: int, embedding_space: int, hidden_size: int,
-                 cfg):
+    def __init__(self, obs_space: int, action_space: int, message_space: int, cfg):
         super(BaselineRNNModel, self).__init__()
         self.cfg = cfg
 
+        obs_space = obs_space * cfg.players
+
         if cfg.use_negotiation:
-            obs_space += message_space
+            obs_space += message_space * cfg.players
 
         if cfg.use_embedding:
-            obs_space += embedding_space
+            obs_space += cfg.embedding_space * cfg.players
 
-        self.rnn = nn.GRUCell(obs_space, hidden_size)
-        self.a_policy = nn.Linear(hidden_size, action_space)
-        self.d_policy = nn.Linear(hidden_size, action_space)
-        self.V = nn.Linear(hidden_size, 1)
+        self.rnn = nn.GRUCell(obs_space, cfg.hidden_size)
+        self.a_policy = nn.Linear(cfg.hidden_size, action_space)
+        self.d_policy = nn.Linear(cfg.hidden_size, action_space)
+        self.V = nn.Linear(cfg.hidden_size, 1)
+
+    def get_h(self):
+        return torch.zeros((1, self.cfg.hidden_size))
 
     def forward(self, obs, messages, embeddings, h):
+        obs = torch.cat(obs)
 
         if self.cfg.use_negotiation:
             obs = torch.cat((obs, torch.cat(messages)))
@@ -89,20 +96,22 @@ class BaselineRNNModel(nn.Module):
             obs = torch.cat((obs, torch.cat(embeddings)))
 
         new_h = self.rnn(obs.unsqueeze(0), h)
-        return self.a_policy(new_h[0]), self.d_policy(new_h[0]), self.V(new_h[0]), new_h
+        return self.a_policy(new_h[0]), self.d_policy(new_h[0]), self.V(new_h[0]), new_h.data
 
 
 class BaselineMLPModel(nn.Module):
 
-    def __init__(self, obs_space: int, action_space: int, message_space: int, embedding_space: int, cfg):
+    def __init__(self, obs_space: int, action_space: int, message_space: int, cfg):
         super(BaselineMLPModel, self).__init__()
         self.cfg = cfg
 
+        obs_space = obs_space * cfg.players
+
         if cfg.use_negotiation:
-            obs_space += message_space
+            obs_space += message_space * cfg.players
 
         if cfg.use_embedding:
-            obs_space += embedding_space
+            obs_space += cfg.embedding_space * cfg.players
 
         self.linear = nn.Sequential(
             nn.Linear(obs_space, obs_space // 2),
@@ -112,7 +121,11 @@ class BaselineMLPModel(nn.Module):
         self.d_policy = nn.Linear(obs_space // 2, action_space)
         self.V = nn.Linear(obs_space // 2, 1)
 
+    def get_h(self):
+        return None
+
     def forward(self, obs, messages, embeddings, h):
+        obs = torch.cat(obs)
 
         if self.cfg.use_negotiation:
             obs = torch.cat((obs, torch.cat(messages)))
@@ -121,19 +134,25 @@ class BaselineMLPModel(nn.Module):
             obs = torch.cat((obs, torch.cat(embeddings)))
 
         obs = self.linear(obs)
-        return self.a_policy(obs), self.d_policy(obs), self.V(obs)
+        return self.a_policy(obs), self.d_policy(obs), self.V(obs), None
 
 
 class DecisionModel(nn.Module):
 
-    def __init__(self, obs_space: int, action_space: int, message_space: int, embedding_space: int, hidden_size: int,
-                 cfg, model_type):
+    def __init__(self, obs_space: int, action_space: int, message_space: int, cfg, model_type):
         super(DecisionModel, self).__init__()
 
         if model_type == cfg.ModelType.baseline_mlp:
-            self.model = BaselineMLPModel(obs_space, action_space, message_space, embedding_space, cfg)
+            self.model = BaselineMLPModel(obs_space, action_space, message_space, cfg)
+        elif model_type == cfg.ModelType.baseline_rnn:
+            self.model = BaselineRNNModel(obs_space, action_space, message_space, cfg)
+        elif model_type == cfg.ModelType.attention:
+            self.model = AttentionModel(obs_space, action_space, message_space, cfg)
         else:
             raise Exception(f'Model type {model_type.name} has no implementation')
 
+    def get_h(self):
+        return self.model.get_h()
+
     def forward(self, obs, messages, embeddings, h):
-        pass
+        return self.model(obs, messages, embeddings, h)
