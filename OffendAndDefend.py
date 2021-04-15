9eb9bec8a5a95bd37ddd24665360438a14e757eb
Evgenii Krikunov <evgenii.krikunov@pr3vision.com>
2021-04-15 14:49:20 +0300

    env now always zero_sum + add SME Elo implement

--- OffendAndDefend.py
+++ OffendAndDefend.py
@@ -13,9 +13,8 @@ class OADEnvironment:
     ELO_BASE: float = 10.
     ELO_DEL: float = 400.
 
-    def __init__(self, players: int, zero_sum: bool = True, debug: bool = False):
+    def __init__(self, players: int, debug: bool = False):
         self.players = players
-        self.is_zero_sum = zero_sum
         self.debug = debug
         self.obs_space = 2 * (players + 1)
         self.elo = np.full(players, 1000, dtype=np.int)
@@ -49,34 +48,45 @@ class OADEnvironment:
             obs[p][choices[p][self.OFFEND_ID]] = 1.
             obs[p][self.players + 1 + choices[p][self.DEFEND_ID]] = 1.
 
-        if self.is_zero_sum:
-            winners_mask = rewards == self.WIN_REWARD
-            total_win_reward = np.sum(winners_mask)
-            if total_win_reward != 0:
-                rewards[winners_mask] = 1. / total_win_reward
+        # zero sum
+        winners_mask = rewards == self.WIN_REWARD
+        total_win_reward = np.sum(winners_mask)
+        if total_win_reward != 0:
+            rewards[winners_mask] = 1. / total_win_reward
 
-            loosers_mask = rewards == self.LOOSE_REWARD
-            total_loose_reward = np.sum(loosers_mask)
-            if total_loose_reward != 0:
-                rewards[loosers_mask] = -1. / total_loose_reward
+        loosers_mask = rewards == self.LOOSE_REWARD
+        total_loose_reward = np.sum(loosers_mask)
+        if total_loose_reward != 0:
+            rewards[loosers_mask] = -1. / total_loose_reward
 
-            # ELO
-            elo_sum = np.sum(self.elo)
-            estimates = ((elo_sum - self.elo) / (self.players - 1)) - self.elo  # mean R other - R current
-            estimates = 1. / (1. + self.ELO_BASE**(estimates / self.ELO_DEL))  # sigmoid
-
-            # S - E
-            if total_loose_reward == 0:
-                estimates = 1 - estimates
-            elif total_win_reward == 0:
-                estimates = -estimates
+        # http://www.tckerrigan.com/Misc/Multiplayer_Elo/
+        # Simple Multiplayer Elo (SME)
+        '''
+        # Use right or left player to correct your elo
+        argind = np.argsort(self.elo)
+        estimates = -self.elo
+        for i in range(len(argind)):
+            cur_player = argind[i]
+            if rewards[cur_player] > 0.:
+                vs_player = cur_player if i == 0 else argind[i - 1]
             else:
-                scores = np.zeros(self.players)
-                scores[rewards > 0.] = 1.
-                estimates = scores - estimates
+                vs_player = cur_player if i == (len(argind) - 1) else argind[i + 1]
+            estimates[cur_player] += estimates[vs_player]
+        '''
 
-            # correct rating
-            self.elo = self.elo + (self.ELO_K * estimates).astype(np.int)
+        # Use mean elo of other players to correct your elo
+        elo_sum = np.sum(self.elo)
+        estimates = ((elo_sum - self.elo) / (self.players - 1)) - self.elo  # mean R other - R current
+
+        estimates = 1. / (1. + self.ELO_BASE**(estimates / self.ELO_DEL))  # sigmoid
+
+        # S - E
+        scores = np.zeros(self.players)
+        scores[rewards > 0.] = 1.
+        estimates = scores - estimates
+
+        # compute rating
+        self.elo = self.elo + (self.ELO_K * estimates).astype(np.int)
 
         self.__print(f'Награды {rewards}')
         self.__print(f'ELO {self.elo}')
