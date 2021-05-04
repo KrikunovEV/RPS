fix gradients

Agent.py
@@ -18,21 +18,21 @@ class Agent:
         list_params = list(self.model.parameters())
 
         self.transformer = None
-        self.start_messages = dict()
+        self.start_kv = []
+        self.kv = []
         if cfg.negotiation.use:
             self.transformer = []
             for step in range(self.negotiation_steps):
                 self.transformer.append(AttentionLayer(cfg))
                 list_params = list_params + list(self.transformer[-1].parameters())
 
-            for p in range(cfg.common.players):
-                if p != id:
                     #if self.negotiable:
                     #    self.start_messages[str(p)] = torch.nn.Parameter(torch.randn(cfg.negotiation.space,
                     #                                                                 requires_grad=True))
                     #    list_params = list_params + [self.start_messages[str(p)]]
                     #else:
-                    self.start_messages[str(p)] = torch.zeros(cfg.negotiation.space)
+                self.start_kv = torch.stack([torch.zeros(cfg.negotiation.space) for p in range(cfg.common.players - 1)])
+            self.kv = self.start_kv
 
         self.optimizer = optim.Adam(list_params, lr=cfg.train.lr)
 
@@ -55,26 +55,25 @@ class Agent:
     def reset_memory(self):
         pass
 
-    def negotiate(self, kv, q, step):
-        new_messages = self.start_messages
-
+    def negotiate(self, q, step):
         if self.negotiable and step < self.negotiation_steps:
 
+            if step == 0:
+                self.kv = self.start_kv
+
+            print(q.shape, self.kv.shape)
+
             # concatenate one-hot vector to encode agent position
-            emb = torch.eye(len(kv))
-            kv = torch.cat((emb, kv), dim=1)
+            emb = torch.eye(len(self.kv))
+            kv = torch.cat((emb, self.kv), dim=1)
             q = torch.cat((emb, q), dim=1)
 
-            messages = self.transformer[step](kv, q)
-            ind = 0
-            for p in range(self.cfg.common.players):
-                if p != self.id:
-                    new_messages[str(p)] = messages[ind]
-                    ind += 1
+            self.kv = self.transformer[step](kv, q)
 
-        return new_messages
+    def make_decision(self, q, epsilon):
+        if self.cfg.negotiation.use:
+            obs = torch.cat((q, self.kv), dim=1)
 
-    def make_decision(self, obs, epsilon):
         a_logits, d_logits, V = self.model(obs)
 
         a_policy = functional.softmax(a_logits, dim=-1)
@@ -97,10 +96,10 @@ class Agent:
             d_entropy = (d_policy * torch.log_softmax(d_logits, dim=-1)).sum()
             self.entropy.append(a_entropy + d_entropy)
         else:
-            self.attacks_metric[a_action] += 1
-            self.defends_metric[d_action] += 1
+            self.attacks_metric[a_action if a_action < self.id else (a_action + 1)] += 1
+            self.defends_metric[d_action if d_action < self.id else (d_action + 1)] += 1
 
-        return [a_action, d_action]
+        return [a_action if a_action < self.id else (a_action + 1), d_action if d_action < self.id else (d_action + 1)]
 
     def rewarding(self, reward):
         if self.eval:

Model.py
@@ -100,7 +100,7 @@ class SiamMLPModel(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(obs_space // 2, 2),
         )
-        self.V = nn.Linear(2 * action_space, 1)
+        self.V = nn.Linear(2 * (action_space - 1), 1)
 
     def forward(self, obs):
         actions = self.policies(obs).reshape(-1)

Orchestrator.py
@@ -10,12 +10,12 @@ class Orchestrator:
         self.eval = False
         self.negotiation_steps = np.max(cfg.negotiation.steps)
         self.shuffle_id = np.arange(cfg.common.players)
-        self.kv_all, self.q_all = None, None
+        self.q_all = None
 
         if not cfg.common.use_obs:
             obs_space = 0
         if cfg.negotiation.use:
-            obs_space += cfg.negotiation.space * 4
+            obs_space += cfg.negotiation.space * 2
         if cfg.embeddings.use:
             obs_space += cfg.embeddings.space
 
@@ -40,29 +40,32 @@ class Orchestrator:
             agent.set_eval(eval)
 
     def negotiation(self):
-        kv_all, q_all = self.__build_kv_q()
+        q_all = self.__build_q()
 
         for step in range(self.negotiation_steps):
-            new_messages = [agent.negotiate(kv_all[agent.id], q_all[agent.id], step) for agent in self.Agents]
-            kv_all, q_all = self.__build_kv_q(new_messages)
+            for agent in self.Agents:
+                agent.negotiate(q_all[agent.id], step)
+            q_all = self.__build_q(start=False)
+            print(len(q_all), q_all[0].shape)
 
-        self.kv_all, self.q_all = kv_all, q_all
+        self.q_all = q_all
 
-    def __build_kv_q(self, messages=None):
-        kv_all, q_all = [], []
+    def __build_q(self, start: bool = True):
+        q_all = []
+        ind = [0, 0, 0]
         for p1 in range(self.cfg.common.players):
-            kv_p, q_p = [], []
+            q_p = []
             for p2 in range(self.cfg.common.players):
-                if p1 != p2:
-                    if messages is None:
-                        kv_p.append(self.Agents[p1].start_messages[str(p2)])
-                        q_p.append(self.Agents[p2].start_messages[str(p1)])
-                    else:
-                        kv_p.append(messages[p1][str(p2)])
-                        q_p.append(messages[p2][str(p1)])
-            kv_all.append(torch.stack(kv_p))
-            q_all.append(torch.stack(q_p).detach())
-        return kv_all, q_all
+                if p1 == p2:
+                    continue
+                if start:
+                    q_p.append(self.Agents[p2].start_kv[ind[p2]].detach())
+                else:
+                    q_p.append(self.Agents[p2].kv[ind[p2]].detach())
+                ind[p2] += 1
+
+            q_all.append(torch.stack(q_p))
+        return q_all
 
     def decisions(self, obs, epsilon):
         if self.cfg.common.use_obs:
@@ -70,20 +73,13 @@ class Orchestrator:
         else:
             obs = torch.empty((0,))
 
+        '''
         if self.cfg.negotiation.use:
             self.q_all = torch.stack([self.q_all[p].reshape(-1) for p in range(len(self.q_all))])
+            obs = torch.cat((obs, self.q_all), dim=1)
+        '''
 
-            choices = []
-            for agent in self.Agents:
-                kv_all_detached = []
-                for p in range(self.cfg.common.players):
-                    kv_all_detached.append(self.kv_all[p].reshape(-1))
-                    if p != agent.id:
-                        kv_all_detached[-1] = kv_all_detached[-1].detach()
-                kv_all_detached = torch.stack(kv_all_detached)
-                agent_obs = torch.cat((obs, kv_all_detached, self.q_all), dim=1)
-
-                choices.append(agent.make_decision(agent_obs, epsilon))
+        choices = [agent.make_decision(self.q_all[agent.id], epsilon) for agent in self.Agents]
 
         if self.eval:
             for p1 in range(self.cfg.common.players - 1):